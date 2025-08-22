797539d63e0e9e8d11e7dd0abfac3b371c54df9c
Thanva Phupingbut <rufiodinoto244@gmail.com>
2025-08-22 07:26:11 +0700

    Update db_listener.py

--- listeners/db_listener.py
+++ listeners/db_listener.py
@@ -1,1 +1,13 @@
+import sqlite3, time
 
+def db_poll_loop(db_file, callback):
+    conn = sqlite3.connect(db_file)
+    cur = conn.cursor()
+    while True:
+        cur.execute("SELECT * FROM messages WHERE processed=0")
+        rows = cur.fetchall()
+        for row in rows:
+            callback(row[1])
+            cur.execute("UPDATE messages SET processed=1 WHERE id=?", (row[0],))
+        conn.commit()
+        time.sleep(2)
